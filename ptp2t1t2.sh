b9760090cc69d041e8be123ec7108b833ec58d26
Jagmeet Singh Hanspal <jagmeet.hanspal@gmail.com>
2021-06-09 22:40:22 +0530

    Colon to decimal

--- ptp2t1t2.sh
+++ ptp2t1t2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-cat ptplog.txt | grep -i sync | cut -d'(' -f3 | cut -d')' -f1 > t1.txt
-cat ptplog.txt | grep -i sync | cut -d'(' -f2 | cut -d')' -f1 > t2.txt
+cat ptplog.txt | grep -i sync | cut -d'(' -f3 | cut -d')' -f1 | tr ':' . > t1.txt
+cat ptplog.txt | grep -i sync | cut -d'(' -f2 | cut -d')' -f1 | tr ':' . > t2.txt
 echo "t1,t2" > t1t2.csv
 paste t1.txt t2.txt | tr '\t' , >> t1t2.csv
 rm t1.txt
